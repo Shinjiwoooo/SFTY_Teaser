55751bd22b37c675e8d407d9eb3f3e49a7794e67
신지우 <shinjiwoo@ringgames.com>
2022-11-21 17:31:12 +0900

    게임소개홈페이지

---
+++ js/event.js
@@ -0,0 +1,151 @@
+
+const playBtn = document.querySelector('.arrow')
+const youTubePopup = document.querySelector('.youTube')
+const linkPopup = document.querySelector('.user_acquisition')
+const closePopup_2 = document.querySelector('.exit_2')
+const closePopup = document.querySelector('.exit')
+const m_menubar = document.querySelector('.m_menubar')
+const openMobileMenu = document.querySelector('.m_menu')
+const mobile_content  = document.querySelector('.gnb_wrap')
+const closeMobileMenu = document.querySelector('.m_close')
+const m_menu_target = document.querySelectorAll('.m_content a')
+// querySelectorAll 은 배열형태로 잡힌다.
+const pc_menu_target = document.querySelectorAll('.pc_menu li')
+let targetBody = document.querySelector('body');
+
+
+
+function show_popup(){
+    youTubePopup.classList.remove("close")
+}
+
+function close_popup(){
+    youTubePopup.classList.add("close")
+   
+}
+
+function close_linkpopup(){
+    linkPopup.classList.add("close")
+}
+
+function open_menu(){
+    openMobileMenu.style.display = "block"
+    openMobileMenu.classList.add("right_fade_in")
+    m_menubar.style.display = "none"
+
+    if (mobile_content != undefined && mobile_content != null) {
+        mobile_content.style.right = "0"
+    }
+    
+
+    
+    targetBody.style.overflow = 'hidden';
+    targetBody.style.touchAction="none";
+    
+}
+
+function scrollWall(e) {
+    console.log(e)
+}
+
+function close_menu(){
+    openMobileMenu.classList.remove("right_fade_in")
+    m_menubar.style.display = "block";
+    targetBody.style.overflow = 'visible';
+    
+    for(var i = 0; i < m_menu_target.length; i++){
+        m_menu_target[i].classList.remove('line')
+    }
+
+}
+
+//처음에 한마디로 박아놓고 시작한다 li로 눌렀을때도 링크가 타야힌다
+function pc_menu_active() {
+    let linkData;
+    for (var i = 0; i < pc_menu_target.length; i++) {
+        if (pc_menu_target[i] == this) {
+            /*
+                클릭한 메뉴가 WhitePaper 또는 Maketplace 라면 ?
+
+                line 이라는 클래스를 넣지 않는다.
+            */
+            
+            if (this.classList.value == 'pc_menu_07' || this.classList.value == 'pc_menu_08') {
+
+            } else {
+                this.classList.add("line");
+            }
+            
+            linkData = this.querySelector('a').href.split("#")[1]
+            // console.log(this.querySelector('a').href)
+            // console.log(this.querySelector('a').href.split("#"))
+             //#을 기점으로 오른쪽 왼쪽  배열로 분리 
+            // console.log(linkData)
+            location.href = `#${linkData}`
+            //`` 백틱안에 쓰는건 전부 string string 안에 변수 추가를 할수 있는데..
+            // `${변수}`=string안에 변수를 넣고 싶을때 $ 씀   #= string
+
+        } else {
+
+            /*
+                클릭한 메뉴가 WhitePaper 또는 Maketplace 라면 ?
+
+                선택된 메뉴들 중에서 line 이라는 클래스를 빼지 않는다.
+                
+                -> 이걸 넣지 않는다면 Members 에서 WhitePaper 를 들어갔다가 홈페이지로
+                다시오면 파란선이 아무데도 없음.
+
+                -> WhitePaper 혹은 Maketplace 메뉴를 들어가기 전에 내가 클릭했던
+                라인은 남아있어야 하기 때문에 넣음.
+            */
+
+            if (this.classList.value == 'pc_menu_07' || this.classList.value == 'pc_menu_08') {
+
+            } else {
+                pc_menu_target[i].classList.remove("line")
+            }
+            
+        }
+    }
+}
+
+function hide_mobile_menu (){
+    
+    if(innerWidth > 768){
+        m_menubar.style.display = "none";
+   
+        openMobileMenu.style.display = "none";
+    }else{
+        m_menubar.style.display = "block";
+    }
+
+    // if(openMobileMenu.style.display == "block") {
+    //     m_menubar.style.display = "none";
+    //     debugger
+    // }
+    
+
+}
+
+//예외처리
+
+for (var i = 0; i < m_menu_target.length; i++) {
+    m_menu_target[i].addEventListener('click', close_menu)
+}
+
+for (var i = 0; i < pc_menu_target.length; i++) {
+    pc_menu_target[i].addEventListener('click', pc_menu_active)
+}
+
+
+playBtn.addEventListener("click", show_popup)
+closePopup.addEventListener("click", close_popup)
+closePopup_2.addEventListener("click",close_linkpopup)
+youTubePopup.addEventListener("click", close_popup)
+m_menubar.addEventListener("click", open_menu)
+closeMobileMenu.addEventListener("click", close_menu)
+window.addEventListener('resize' , hide_mobile_menu)
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,214 @@
+
+// $(function() {p
+  // $(window).resize(function() {
+    var num, valPlus, valPlus1, valPlus2, set, set1, set2;
+    $('#fullpage').fullpage({
+      //options here
+      autoScrolling: true,//  automatic 스크롤을 쓸지 , normal스크롤을 쓸지 정의함
+      scrollHorizontally: true,//슬라이더 안에서 마우스 휠 또는 트랙패드를 써서 수평으로 미끄러지듯 움직일지를 정의합니다.
+      navigation: true, // true로 설정하면 작은 원으로 이루어진 이동 막대기가 나타납니다.
+      navigationTooltips:['Top', 'Game', 'Character', 'Roadmap', 'Partners', 'Members'],
+      showActiveTooltip : false,
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'lastPage'],
+      menu: '#myMenu',
+      responsiveWidth: 1023,//픽셀아래로 부터 fullpage 가깨짐 즉 
+      scrollHorizontally :false,
+      
+      afterLoad: function() {
+        var activeSection = $('.fullpage-wrapper .active');
+        if (activeSection.hasClass('section1')) {
+          if (window.innerWidth < 769) {
+            $(".m_menu01").addClass("line")
+            $(".m_menu02").removeClass("line")
+            $(".m_menu03").removeClass("line")
+            $(".m_menu04").removeClass("line")
+            $(".m_menu05").removeClass("line")
+            $(".m_menu06").removeClass("line")
+          }
+          $("header").css({"background" : "transparent"})
+          // $(".section1").addClass("on");
+          $(".pc_menu li")[0].className = "pc_menu_01 line";
+          $(".pc_menu li")[1].className = "pc_menu_02";
+          $(".pc_menu li")[2].className = "pc_menu_03";
+          $(".pc_menu li")[3].className = "pc_menu_04";
+          $(".pc_menu li")[4].className = "pc_menu_05";
+          $(".pc_menu li")[5].className = "pc_menu_06";
+          $(".pc_menu li")[6].className = "pc_menu_07";
+
+        } else if (activeSection.hasClass('section2')) {
+          
+          if (window.innerWidth < 769) {
+            $(".m_menu01").removeClass("line")
+            $(".m_menu02").addClass("line")
+            $(".m_menu03").removeClass("line")
+            $(".m_menu04").removeClass("line")
+            $(".m_menu05").removeClass("line")
+            $(".m_menu06").removeClass("line")
+          }
+          
+          $("header").css({"background" : "rgba(0, 0, 0, 0.5)"})
+          // $(".section2").addClass("on");
+
+          fadeIn()
+
+          $(".pc_menu li")[0].className = "pc_menu_01";
+          $(".pc_menu li")[1].className = "pc_menu_02 line";
+          $(".pc_menu li")[2].className = "pc_menu_03";
+          $(".pc_menu li")[3].className = "pc_menu_04";
+          $(".pc_menu li")[4].className = "pc_menu_05";
+          $(".pc_menu li")[5].className = "pc_menu_06";
+          $(".pc_menu li")[6].className = "pc_menu_07";
+
+
+        } else if (activeSection.hasClass('section3')) {
+          
+          if (window.innerWidth < 769) {
+            $(".m_menu01").removeClass("line")
+            $(".m_menu02").removeClass("line")
+            $(".m_menu03").addClass("line")
+            $(".m_menu04").removeClass("line")
+            $(".m_menu05").removeClass("line")
+            $(".m_menu06").removeClass("line")
+          }
+          
+          $("header").css({"background" : "rgba(0, 0, 0, 0.5)"})
+
+          $(".pc_menu li")[0].className = "pc_menu_01";
+          $(".pc_menu li")[1].className = "pc_menu_02";
+          $(".pc_menu li")[2].className = "pc_menu_03 line";
+          $(".pc_menu li")[3].className = "pc_menu_04";
+          $(".pc_menu li")[4].className = "pc_menu_05";
+          $(".pc_menu li")[5].className = "pc_menu_06";
+          $(".pc_menu li")[6].className = "pc_menu_07";
+
+        } else if (activeSection.hasClass('section4')) {
+
+          if (window.innerWidth < 769) {
+            $(".m_menu01").removeClass("line")
+            $(".m_menu02").removeClass("line")
+            $(".m_menu03").removeClass("line")
+            $(".m_menu04").addClass("line")
+            $(".m_menu05").removeClass("line")
+            $(".m_menu06").removeClass("line")
+          }
+
+          $("header").css({"background" : "rgba(0, 0, 0, 0.5)"})
+
+          $(".pc_menu li")[0].className = "pc_menu_01";
+          $(".pc_menu li")[1].className = "pc_menu_02";
+          $(".pc_menu li")[2].className = "pc_menu_03";
+          $(".pc_menu li")[3].className = "pc_menu_04 line";
+          $(".pc_menu li")[4].className = "pc_menu_05";
+          $(".pc_menu li")[5].className = "pc_menu_06";
+          $(".pc_menu li")[6].className = "pc_menu_07";
+
+        } else if (activeSection.hasClass('section6')) {
+          
+          if (window.innerWidth < 769) {
+            $(".m_menu01").removeClass("line")
+            $(".m_menu02").removeClass("line")
+            $(".m_menu03").removeClass("line")
+            $(".m_menu04").removeClass("line")
+            $(".m_menu05").addClass("line")
+            $(".m_menu06").removeClass("line")
+          }
+          
+          $("header").css({"background" : "rgba(0, 0, 0, 0.5)"})
+
+          $(".pc_menu li")[0].className = "pc_menu_01";
+          $(".pc_menu li")[1].className = "pc_menu_02";
+          $(".pc_menu li")[2].className = "pc_menu_03";
+          $(".pc_menu li")[3].className = "pc_menu_04";
+          $(".pc_menu li")[4].className = "pc_menu_05 line";
+          $(".pc_menu li")[5].className = "pc_menu_06";
+          $(".pc_menu li")[6].className = "pc_menu_07";
+
+        } else if (activeSection.hasClass('section5')) {
+          if (window.innerWidth < 769) {
+            $(".m_menu01").removeClass("line")
+            $(".m_menu02").removeClass("line")
+            $(".m_menu03").removeClass("line")
+            $(".m_menu04").removeClass("line")
+            $(".m_menu05").removeClass("line")
+            $(".m_menu06").addClass("line")
+          }
+          $("header").css({"background" : "rgba(0, 0, 0, 0.5)"})
+
+          $(".pc_menu li")[0].className = "pc_menu_01";
+          $(".pc_menu li")[1].className = "pc_menu_02";
+          $(".pc_menu li")[2].className = "pc_menu_03";
+          $(".pc_menu li")[3].className = "pc_menu_04";
+          $(".pc_menu li")[4].className = "pc_menu_05";
+          $(".pc_menu li")[5].className = "pc_menu_06 line";
+          $(".pc_menu li")[6].className = "pc_menu_07";
+
+        } else if (activeSection.hasClass('section7')) {
+
+            if (window.innerWidth < 769) {
+              $(".m_menu01").removeClass("line")
+              $(".m_menu02").removeClass("line")
+              $(".m_menu03").removeClass("line")
+              $(".m_menu04").removeClass("line")
+              $(".m_menu05").removeClass("line")
+              $(".m_menu06").removeClass("line")
+              $(".m_menu07").removeClass("line")
+              
+            }
+            $("header").css({"background" : "rgba(0, 0, 0, 0.5)"})
+            // $(".section7").addClass("on");
+
+            $(".pc_menu li")[0].className = "pc_menu_01";
+            $(".pc_menu li")[1].className = "pc_menu_02";
+            $(".pc_menu li")[2].className = "pc_menu_03";
+            $(".pc_menu li")[3].className = "pc_menu_04";
+            $(".pc_menu li")[4].className = "pc_menu_05";
+            $(".pc_menu li")[5].className = "pc_menu_06";
+            $(".pc_menu li")[6].className = "pc_menu_07";
+        } else if (activeSection.hasClass('section8')) {
+          if (window.innerWidth < 769) {
+            $(".m_menu01").removeClass("line")
+            $(".m_menu02").removeClass("line")
+            $(".m_menu03").removeClass("line")
+            $(".m_menu04").removeClass("line")
+            $(".m_menu05").removeClass("line")
+            $(".m_menu06").removeClass("line")
+            $(".m_menu07").removeClass("line")
+            $(".m_menu08").removeClass("line")
+          }
+          $("header").css({"background" : "rgba(0, 0, 0, 0.5)"})
+
+          $(".pc_menu li")[0].className = "pc_menu_01";
+          $(".pc_menu li")[1].className = "pc_menu_02";
+          $(".pc_menu li")[2].className = "pc_menu_03";
+          $(".pc_menu li")[3].className = "pc_menu_04";
+          $(".pc_menu li")[4].className = "pc_menu_05";
+          $(".pc_menu li")[5].className = "pc_menu_06 ";
+          $(".pc_menu li")[6].className = "pc_menu_07";
+          $(".pc_menu li")[7].className = "pc_menu_08";
+
+        }
+        
+
+      }
+    });
+
+  // }).trigger("resize");
+    
+  function fadeIn() {
+    
+    $(".fadeIn01").attr('id', '');
+    // setTimeout -> 비동기 함수
+    setTimeout(() => {
+      $(".fadeIn02").attr('id', '');
+    }, 250)
+
+    setTimeout(() => {
+      $(".fadeIn03").attr('id', '');
+    }, 500)
+
+    setTimeout(() => {
+      $(".fadeIn04").attr('id', '');
+    }, 750)
+
+  }
+  
